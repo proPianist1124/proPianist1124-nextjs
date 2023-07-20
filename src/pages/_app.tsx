347edf108ad9a31f2f4a2bd450bb17e0897e1da5
proPianist1124 <phantomforces11242009@gmail.com>
2023-07-20 09:26:35 -0700

    updated og description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,9 +14,9 @@ export default function App({ Component, pageProps }: AppProps) {
       sizes="<generated>"
       />
       <meta property="og:title" content="proPianist1124" />
-      <meta name="theme-color" content="#ffffff"/>
       <meta property="og:image" content="https://res.cloudinary.com/dyemytgtv/image/upload/v1689869843/propianist1124.png" />
-      <meta name="description" content="proPianist1124 - Fullstack Nextjs and Nodejs developer who loves designing webpages and webapps!"/>
+      <meta property="og:description" content="proPianist1124 - Fullstack Nextjs and Nodejs developer who loves designing webpages and webapps!"/>
+      <meta name="theme-color" content="#ffffff"/>
     </Head>
     <Script src="https://kit.fontawesome.com/09556a902e.js" />
     <Component {...pageProps} />
