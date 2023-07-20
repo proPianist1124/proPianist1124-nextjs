f2ce2bfded3a51a8684c0e3c00308e8a365cdfab
proPianist1124 <phantomforces11242009@gmail.com>
2023-07-20 09:19:42 -0700

    added custom favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head"
 import Script from "next/script"
 import '@/styles/styles.css'
 import type { AppProps } from 'next/app'
@@ -5,6 +6,14 @@ import type { AppProps } from 'next/app'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+    <Head>
+      <link
+      rel="icon"
+      href="https://res.cloudinary.com/dyemytgtv/image/upload/v1689869843/propianist1124.png"
+      type="image/<generated>"
+      sizes="<generated>"
+      />
+    </Head>
     <Script src="https://kit.fontawesome.com/09556a902e.js" />
     <Component {...pageProps} />
     </>
