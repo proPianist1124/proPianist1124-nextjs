aca51a70e8b84333e2f0850ed185c82b33156d64
proPianist1124 <phantomforces11242009@gmail.com>
2023-07-24 12:38:22 -0700

    new ui update

--- src/pages/blog/5-10-2023.tsx
+++ src/pages/blog/5-10-2023.tsx
@@ -16,9 +16,9 @@ export default function may102023(){
 		</div>
 		<br></br>
 		<div style = {{textAlign:"left", margin:"40px", lineHeight:"35px",}}>
-			<h4>
+			<p>
 				next.js was a main component of this site because its default fast performance lightweight framework design. my <Link href = "https://replit.com/@proPianist1124/proPianist1124-html?v=1">previous personal webpage</Link> was too complicated and was made out of pure HTML & CSS, as well as iFrames (incredibly insufficient). i also wanted to create a minamalist personal webpage, like many others in the <Link href = "https://discord.gg/friendsofreplit">Replit Discord</Link>. However, my main inspiration was from <Link href = "https://coder100.vercel.app">Coder100&#39;s webpage</Link> because of its simplicity, as well as its extremely fast loading speed and website performance.
-			</h4>
+			</p>
 			</div>
 	</center>
 	</>
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -15,33 +15,34 @@ export default function Contact(){
 			<span className = "head">📱 Contact</span>
 		</div>
 		<div className = "contact">
-			<h2>Replit</h2>
+			<p>Replit</p>
 			<Link href = "https://replit.com/@proPianist1124" className = "replit">proPianist1124</Link>
-			<h2>Discord</h2>
+			<br></br><br></br>
+			<p>Discord</p>
 			<Link href = "https://discord.com/users/844689790075011083" className = "discord">chicken.poop</Link>
 			<br></br><br></br>
-			<h2>Email</h2>
+			<p>Email</p>
 			<Link href = "mailto:invisible.horse1124@proton.me" className = "email">invisible.horse1124@proton.me</Link>
 			<br></br><br></br>
-			<h2>Twitter</h2>
+			<p>Twitter</p>
 			<Link href = "https://twitter.com/elonmusk" className = "twitter">happypeng_1</Link>
 			<br></br><br></br>
-			<h2>Yofro</h2>
-			<Link href = "https://yofro.vercel.app/@proPianist1124" className = "big-space">proPianist1124</Link>
+			<p>Yofro</p>
+			<Link href = "https://yofro.vercel.app/@proPianist1124" className = "yofro">proPianist1124</Link>
 			<br></br><br></br>
-			<h2>Social Rumbles</h2>
+			<p>Social Rumbles</p>
 			<Link href = "https://socialrumbles.com/@greenSmiley" className = "socialRumbles">greenSmiley</Link>
 			<br></br><br></br>
-			<h2>Youtube</h2>
+			<p>Youtube</p>
 			<Link href = "https://www.youtube.com/channel/UCACuB_DHqEQ-SpXYMmWFmyw" className = "youtube">Phantom Studios</Link>
 			<br></br><br></br>
-			<h2>Github</h2>
+			<p>Github</p>
 			<Link href = "https://github.com/propianist1124" className = "github">proPianist1124</Link>
 			<br></br><br></br>
-			<h2>Spotify</h2>
+			<p>Spotify</p>
 			<Link href = "https://open.spotify.com/user/31vq6ry37tfa2qntvk6yjcf32cry" className = "spotify">Doggo</Link>
 			<br></br><br></br>
-			<h2>Reddit</h2>
+			<p>Reddit</p>
 			<Link href = "https://www.reddit.com/user/simonsays1124" className = "reddit">simonSays1124</Link>
 		</div>
 		<br></br><br></br>
